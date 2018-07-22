ac671fad5991a0ac01f503e2789905cc959d5c58
Ivica Rade <ivica.rade3@gmail.com>
2018-07-22 11:35:49 +0200

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,24 @@
+//check off specific todos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//click on X to delete a listing
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    //stops event bubbling
+    event.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing todo text from input, clears the input
+        var todoText = $(this).val();
+        $(this).val("");
+        //creates a new li and adds to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1>jQuery todo app</h1>
+
+A small todo list project created in jQuery
